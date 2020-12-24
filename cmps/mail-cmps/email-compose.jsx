afe9b88c2fd6280e8298e6578d9fda5e4c03732a
aderajoe tsegay <adetsegay@gmail.com>
2020-12-24 14:37:13 +0200

    add new mail to storage

--- cmps/mail-cmps/email-compose.jsx
+++ cmps/mail-cmps/email-compose.jsx
@@ -4,63 +4,60 @@ import { mailService } from "../../services/mail-services/mail-service.js";
 
 
 
-export class NewMail extends React.Component{
+export class NewMail extends React.Component {
 
-state={
-mail:{idx: 4,
-    sentTo:'',
-    subject: '',
-    body: 'what time is it?',
-    isRead: false,
-    sentAt: 1551133930594}
-}
-
-
-
-
-    createNewMail(newM){
-        console.log('new mail')
-        var mails = mailService.query()
-         // .then(books => this.setState({ notes })) when use promise.
-         console.log(newM)
-         mails.push(newM)
-        //  this.setState({mails})
+    state = {
+        mail: {
+            idx: 4,
+            sentTo: '',
+            subject: '',
+            body: 'what time is it?',
+            isRead: false,
+            sentAt: 1551133930594
+        }
     }
-    
+
+
+
+
+    createNewMail = () => {
+        console.log('new mail')
+        let newMail = { ...this.state.mail };
+        console.log(newMail)
+        let mails = mailService.query()
+        // .then(books => this.setState({ notes })) when use promise.
+        // console.log(newM)
+        //  mails.push(this.state.mail)
+        newMail.sentAt= Date.now()
+         this.setState({mails})
+         mailService.sendMail(newMail)
+         return mails
+    }
+
     onInputChange = (ev) => {
         const value = ev.target.value
         const mailCopy = { ...this.state.mail };
-        mailCopy[ev.target.subject] = value;
-        console.log(mailCopy)
-
-
+        // console.log(mailCopy)
+        
         const inputType = ev.target.name
-        let type;
-        // if(inputType=== 'address'){
-        //     // type = 'sentTo'
+        if (inputType=== 'subject'){
 
-        // }else if(inputType === 'subject'){
-        //     type = 'subject'
-        // }else{
-        //     type = 'content'
-        // }
+            mailCopy.subject = value;
+        }else if (inputType=== 'content'){
+            mailCopy.body = value;
+
+        }else{
+            mailCopy.sentTo = value;
+
+        }
+        
+        
+        
+  
 
         this.setState({
-           mail: mailCopy
-
-
+            mail: mailCopy
         });
-
-
-
-
-        console.log(value)
-
-        // mailCopy[ev.target.] = value;
-
-
-        // const reviewCopy = { ...this.state.review };
-        // reviewCopy[ev.target.name] = value;
 
     }
 
@@ -68,19 +65,19 @@ mail:{idx: 4,
 
 
 
-render(){
+    render() {
 
 
-          return <div>
-              <input  onChange={this.onInputChange} type="email" name="address" placeholder="Write to" />
-              <input  onChange={this.onInputChange} type="text" name="subject" placeholder="Subject" />
-              <input  onChange={this.onInputChange} type="text" name="content" placeholder="content" />
+        return <div>
+            <input onChange={this.onInputChange} type="email" name="address" placeholder="Write to" />
+            <input onChange={this.onInputChange} type="text" name="subject" placeholder="Subject" />
+            <input onChange={this.onInputChange} type="text" name="content" placeholder="content" />
 
-                
-                
-                <button onClick={this.createNewMail} >send</button> 
-            </div>
-}
+
+
+            <button onClick={this.createNewMail} >send</button>
+        </div>
+    }
 
 
 
